90f4188a21d4f94828c970d011f8dece5c0becec
liuqiqi <liu.qiqi01@iwhalecloud.com>
2020-04-28 16:33:22 +0800

    计数排序

---
+++ src/com/liuqiqi/sort/CountingSort.java
@@ -0,0 +1,46 @@
+package com.liuqiqi.sort;
+
+/**
+ * 计数排序
+ *
+ * @author liuqiqi
+ * @date 2020/4/28 16:01
+ */
+public class CountingSort extends BaseReturnOrder {
+
+    private Integer maxNum = null;
+
+    public CountingSort(Integer maxNum) {
+        this.maxNum = maxNum;
+    }
+
+    @Override
+    public Integer[] sort(Integer[] a) {
+        Integer[] indexArray = new Integer[maxNum];
+        Integer[] result = new Integer[a.length];
+        for (int e = 0; e < indexArray.length; e++) {
+            indexArray[e] = 0;
+        }
+        /*indexArray存储的次数赋值*/
+        for (int f = 0; f < a.length; f++) {
+            indexArray[a[f]]++;
+        }
+        /*计算indexArray中元素的位置索引*/
+        for (int i = 0; i < indexArray.length - 1; i++) {
+            indexArray[i + 1] = indexArray[i] + indexArray[i + 1];
+        }
+        /*赋值,注意下标*/
+        for (int j = 0; j < a.length; j++) {
+            result[indexArray[a[j]] - 1] = a[j];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        CountingSort countingSort = new CountingSort(1000);
+        Integer[] data = countingSort.prepareData();
+        Integer[] sort = countingSort.sort(data);
+        System.out.println(countingSort.isSort(sort));
+        countingSort.show(sort);
+    }
+}
