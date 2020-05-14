72d6002e5f553d04cbdeaa58862e338c37dabf19
liuqiqi <liu.qiqi01@iwhalecloud.com>
2020-05-14 23:40:04 +0800

    平衡二叉树

---
+++ src/com/liuqiqi/tree/BinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.liuqiqi.tree;
+
+/**
+ * @author liuqiqi
+ * @date 2020/5/12 18:19
+ */
+public class BinarySearchTree {
+}
---
+++ src/com/liuqiqi/tree/Tree.java
@@ -0,0 +1,8 @@
+package com.liuqiqi.tree;
+
+/**
+ * @author liuqiqi
+ * @date 2020/5/12 18:20
+ */
+public interface Tree {
+}
